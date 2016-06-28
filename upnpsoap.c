2625f64da0cf4210d98d0e02dc1f62a3f4f7fbcb
Nathanael Anderson <nathan@master-technology.com>
2016-06-28 00:32:16 -0500

    Fix SQL issue

--- upnpsoap.c
+++ upnpsoap.c
@@ -1520,9 +1520,9 @@ BrowseContentDirectory(struct upnphttp * h, const char * action)
 
 			sql = sqlite3_mprintf("SELECT %s, %s, %s, " COLUMNS
 		              "from OBJECTS o left join DETAILS d on (d.ID = o.DETAIL_ID)"
-				      " where (%s %s) and (o.password is null or o.password = '' or o.password in (%s))  limit %d, %d;",
+				      " where (%s and (o.password is null or o.password = '' or o.password in (%s)) %s)  limit %d, %d;",
 				      objectid_sql, parentid_sql, refid_sql,
-				      where, THISORNUL(orderBy), args.password ? args.password : "''", StartingIndex, RequestedCount);
+				      where, args.password ? args.password : "''", THISORNUL(orderBy), StartingIndex, RequestedCount);
 			DPRINTF(E_DEBUG, L_HTTP, "Browse SQL: %s\n", sql);
 			ret = sqlite3_exec(db, sql, callback, (void *) &args, &zErrMsg);
 		}
